ed1e1c2fbea44f11fa757a52fadb0c32320d21e9
Arunkumar Ganesan <67154864+arun-lmnas@users.noreply.github.com>
2025-04-18 15:27:08 +0000

    trends temp

--- packages/middleware/src/engine/transformer.ts
+++ packages/middleware/src/engine/transformer.ts
@@ -1,14 +1,61 @@
-import { IsourceType, ItargetType, Itransformer } from "../types/types";
+import { Iquery, IsourceType, ItargetType, Itransformer } from "../types/types";
 
-export abstract class clTransformer<T> implements Itransformer {
+export abstract class clTransformer<T> implements Itransformer<T> {
     contentType: string
     transformationRule: string 
     sourceType: IsourceType
     targetType: ItargetType 
+    query: Iquery<T>
     abstract performTransformation(idSourceData:IsourceType):ItargetType 
+    getData():Promise<T> {
+        return this.query.executeQuery()
+    }
     constructor(iContentType: string) {
         this.contentType = iContentType
+        
     }
 }
 
-export class clTrendsPageTransformer extends clTransformer implements
+export class clTrendsPageTransformer extends clTransformer<ItargetType> {
+    constructor() {
+        super("trends")
+        this.transformationRule = "trends"
+        this.sourceType = {
+            heroSection: {
+                heading: {
+                    title: "",
+                    subtitle: "",
+                    highlight: ""
+                },
+                description: "",
+                buttons: [
+                    {
+                        label: "",
+                        href: "",
+                        icon: "",
+                        formMode: ""
+                    }
+                ]
+            }
+        }
+        this.targetType = {
+            heroSection: {
+                heading: {
+                    title: "",
+                    subtitle: "",
+                    highlight: ""
+                },
+                description: "",
+                buttons: [
+                    {
+                        label: "",
+                        href: "",
+                        icon: "",
+                        formMode: ""
+                    }
+                ]
+            }
+        }
+    }
+
+}
